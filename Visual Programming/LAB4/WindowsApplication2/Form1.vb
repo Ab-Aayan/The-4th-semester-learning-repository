33cd4fb9cc1cff2a523a84da5f2bc10da3365866
@Ab_Aayan <93578138+Ab-Aayan@users.noreply.github.com>
2022-12-13 17:14:41 +0545

    Add files via upload

---
+++ Visual Programming/LAB4/WindowsApplication2/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Then
+            MessageBox.Show("Please Enter the Username & Password")
+            Focus()
+            Exit Sub
+        ElseIf TextBox2.Text = "" Then
+
+            MessageBox.Show("Please Enter the Password")
+            Focus()
+            Exit Sub
+
+
+        End If
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            TextBox2.PasswordChar = ""
+        Else
+            TextBox2.PasswordChar = "*"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        MessageBox.Show("Cancel")
+    End Sub
+End Class
